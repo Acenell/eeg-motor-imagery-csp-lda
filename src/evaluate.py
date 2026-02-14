d85aeea5dd2165bc7e3910c3e6198dc1548b75bd
Allen <darksheep528@gmail.com>
2026-02-14 14:38:22 -0500

    Create evaluate.py

---
+++ src/evaluate.py
@@ -0,0 +1,40 @@
+from sklearn.model_selection import StratifiedGroupKFold, StratifiedKFold, cross_val_score
+
+def evaluate(clf, data):
+    """
+    Evaluates performance of CSP+LDA pipeline with the given data
+    from get_subject_data()
+    
+    Parameters
+    ----------
+    clf : sklearn.pipeline.Pipeline
+        Pipeline containing CSP for feature extraction and LDA
+        for classification.
+    data : dict
+        x : np.ndarray
+            EEG data of shape (n_epochs, n_channels, n_times).
+        y : np.ndarray
+            Labels for each epoch of shape (n_epochs).
+        groups : np.ndarray
+            Numeric run identifier of shape (n_epochs). Differentiate
+            epochs between runs
+
+    Returns
+    ----------
+    mean_acc : float
+        Mean accuracy of all scores
+    scores : np.ndarray
+        Accuracy for each fold
+    """
+
+    x, y , groups = data["x"], data["y"], data["groups"]
+    n_unique_groups = len(np.unique(groups))
+    if n_unique_groups <= 1:
+        cv = StratifiedKFold(n_splits=5, shuffle=True)
+        scores = cross_val_score(clf, x, y, cv=cv)
+    else:
+        cv = StratifiedGroupKFold(n_splits=min(n_unique_groups, 5), shuffle=True)
+        scores = cross_val_score(clf, x, y, groups=groups, cv=cv)
+        
+    print("Test accuracy:", corrects.mean())
+    return scores.mean(), scores
